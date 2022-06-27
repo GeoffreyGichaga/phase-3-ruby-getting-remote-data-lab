a1285f9434503a9107178bc3c1b3a839bb00be45
Git_TJ <98149148+GeoffreyGichaga@users.noreply.github.com>
2022-06-28 02:39:30 +0300

    commit tree f6db23cb6d1f8e54cacc3cc75094f8b4fcbe3cb4

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,26 @@
 # Write your code here
+require 'open-uri'
+require 'net/http'
+require 'json'
+
+class GetRequester 
+    # URL = 'https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json'
+
+    def initialize(url)
+        @url = url 
+    end
+
+    def get_response_body
+        uri = URI.parse(@url)
+        response = Net::HTTP.get_response(uri)
+        response.body
+    end
+
+    def parse_json
+        data = JSON.parse(self.get_response_body)
+    end
+end
+
+get_requester = GetRequester.new('https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json'
+)
+get_requester.parse_json
